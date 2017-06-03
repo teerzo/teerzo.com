2e00292c9321f0e882e55915310c6521a4dcbab3
kyle mcarthur <teerzo@live.com.au>
2017-06-04 00:16:23 +1000

    initial commit - default yeoman/generator-angular https://github.com/yeoman/generator-angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name websiteTeerzoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the websiteTeerzoApp
+ */
+angular.module('websiteTeerzoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
