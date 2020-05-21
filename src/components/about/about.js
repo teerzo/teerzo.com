570432a68c5d0d0f4d92208a189bcddb9dc3bf2a
kyle mcarthur <teerzo@live.com.au>
2020-05-21 16:24:39 +1000

    add smaller profile image for load time testing

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -3,10 +3,17 @@ import React, { useEffect, useState } from 'react';
 import cx from 'classnames';
 
 import imgProfile from '../../images/profile.png';
+import imgProfileSmall from '../../images/profile-small.png';
 
 import './about.scss';
 
 export default function About(props) {
+
+    const [loaded, setLoaded] = useState(true);
+
+    useEffect(() => {
+
+    }, []);
 
     const cmpClasses = cx(
         'about',
@@ -14,10 +21,16 @@ export default function About(props) {
 
     return (
         <div className={cmpClasses}>
+            {/* {loaded ?
+                <img className="profile" src={imgProfile} />
+                :
+                <img className="profile" src={imgProfileSmall} />
+            } */}
 
-            <img className="profile" src={imgProfile}/> 
+            <img className="profile" src={imgProfile} />
+            <img className="profile" src={imgProfileSmall} />
             <p> About </p>
-        
+
         </div>
     );
 }
