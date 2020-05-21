231ccd958d22c6df2835981b30c55e05188a4ef7
kyle mcarthur <teerzo@live.com.au>
2020-05-21 18:11:37 +1000

    add dummy test project data

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -2,9 +2,37 @@
 import React, { useEffect, useState } from 'react';
 import cx from 'classnames';
 
+import Image from '../image';
+
+import imgProfile from '../../images/profile.png';
+import imgProfileSmall from '../../images/profile-small.png';
+
 import './projects.scss';
 
 export default function Projects(props) {
+
+    const [items, setItems] = useState([
+        { 
+            name: 'Test',
+            link: '',   
+        },
+        { 
+            name: 'Test',
+            link: '',   
+        },
+        { 
+            name: 'Test',
+            link: '',   
+        },
+        { 
+            name: 'Test',
+            link: '',   
+        },
+        { 
+            name: 'Test',
+            link: '',   
+        },
+    ]);
 
     const cmpClasses = cx(
         'projects',
@@ -12,44 +40,30 @@ export default function Projects(props) {
 
     return (
         <div className={cmpClasses}>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
-            <p> test </p>
+            {items && items.length > 0 ?
+                <div className="grid">
+                    {items.map((item, key) => {
+                        const itemClasses = cx(
+                            'item',
 
-            <p> BOTTOM </p>
-
+                        )
+                        return (
+                            <div key={key} className={itemClasses}> 
+                                <div className="thumb"> 
+                                    <Image src={imgProfile} preview={imgProfileSmall}/>
+                                </div>
+                                Item 
+                            
+                            </div>
+                        )
+                    })
+                }
+                </div>
+                :
+                <div className="blank">
+                    No projects available
+             </div>
+            }
         </div>
     );
 }
